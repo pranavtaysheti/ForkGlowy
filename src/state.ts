547eda267452f8359357475647f0fa06124cd4c7
pranavtaysheti <pranavtaysheti@gmail.com>
2023-08-31 12:03:30 +0530

    version 0.1.9

--- src/state.ts
+++ src/state.ts
@@ -1,8 +1,11 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { type TypedStartListening, type TypedAddListener, configureStore, createListenerMiddleware, addListener } from "@reduxjs/toolkit";
 import { tasksReducer } from "./state/tasks";
 import { tasksVisiblityReducer } from "./state/tasks_visiblity";
 import { loginUserReducer } from "./state/login_user";
 import { loginDataReducer } from "./state/login_data";
+import { useDispatch } from "react-redux";
+
+const listenerMiddleware = createListenerMiddleware()
 
 export const store = configureStore({
   reducer: {
@@ -16,8 +19,13 @@ export const store = configureStore({
       ignoredActions: ["loginUser/manageUser/fulfilled", "loginUser/setUser"],
       ignoredPaths: ["loginUser.user", "loginUser.userCredential"],
     }
-  })
+  }).prepend(listenerMiddleware.middleware)
 });
 
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
+export const useAppDispatch: () => AppDispatch = useDispatch;
+export type AppStartListening = TypedStartListening<RootState, AppDispatch>;
+export const startAppListening = listenerMiddleware.startListening as AppStartListening;
+export const addAppListener = addListener as TypedAddListener<RootState, AppDispatch>;
+
